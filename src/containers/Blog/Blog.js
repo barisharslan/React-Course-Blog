92a64f6378b2254262323d4130ef66f3928560c8
Barish Arslan <barisharslan42@gmail.com>
2020-04-10 10:58:40 -0700

    first commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+// import axios from '../../axios';
+import Posts from './Posts/Posts'
+import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
+import './Blog.css';
+
+class Blog extends Component {
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink 
+                                to="/" 
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: "/new-post",
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
+            </div>
+        );
+    }
+}
+
+export default Blog;
